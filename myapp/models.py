45227d313f72e697469cdfb924f22e801163b19d
Mihai Gociu <mihai.gociu@soundstack.com>
2023-02-09 10:02:36 +0200

    Update models.py
    Please read about django relationships at https://docs.djangoproject.com/en/4.1/topics/db/examples/
    
    I also have a suggestion that would make the project a bit more interesting. Check the api at https://api.chucknorris.io/#!
    Let's add a CNJoke model where we store the user's preferred jokes. 
    Here's my suggestion:
    - create a CNJokes view; when the user enters the page, you retrieve a random joke from https://api.chucknorris.io/jokes/random and present it to the user; the user has the possibility to "like" the joke (by pressing a Like button on the page or something like that). This should submit a form and create a CNJoke instance for the user.
    - create a LikedJokes view where the user can see the jokes they liked so far.
    - to fetch the jokes from the api you can use the requests python library at https://pypi.org/project/requests/

--- myapp/models.py
+++ myapp/models.py
@@ -3,11 +3,12 @@ from django.contrib.auth.models import User
 
 
 class Item(models.Model):
+    # it seems to me that the item should be linked with the ToDoList via a many-to-one relationship
+    # please read about django relationships at https://docs.djangoproject.com/en/4.1/topics/db/examples/
     list_name = models.CharField(max_length=100, default='Default list name')
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
-
 
     def __str__(self):
         return self.name
